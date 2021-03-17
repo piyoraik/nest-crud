289bf0e08230b67687dfa3c47487e65ca694f023
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-03-17 16:29:52 +0900

    bookのcreate用dtoの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), BookModule],
   controllers: [AppController],
   providers: [AppService],
 })

0543d0406649ebd8e060b4bdb91342eb9108fbb8
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-03-17 16:37:28 +0900

    全て参照・登録用サービス作成

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from 'src/entities/book.entity';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
 
 @Module({
   controllers: [BookController],
-  providers: [BookService]
+  imports: [TypeOrmModule.forFeature([Book])],
+  providers: [BookService],
 })
 export class BookModule {}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from 'src/entities/book.entity';
+import { InsertResult, Repository } from 'typeorm';
+import { CreateBookDto } from './dto/book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(
+    @InjectRepository(Book) private readonly book: Repository<Book>,
+  ) {}
+
+  async findAll(): Promise<Book[]> {
+    return await this.book.find();
+  }
+
+  async create(book: CreateBookDto): Promise<InsertResult> {
+    return await this.book.insert(book);
+  }
+}
